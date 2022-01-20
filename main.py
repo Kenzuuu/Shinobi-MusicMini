b042f92f40878d3e6ae128314929fad37f310217
SankiWizer <sankiwizer@gmail.com>
2022-01-20 17:14:19 +0530

    Kaal VC User Bot

---
+++ main.py
@@ -0,0 +1,21 @@
+import asyncio
+from pytgcalls import idle
+from config import call_py
+from KaalMusic.quote import arq
+
+
+async def main():
+    await call_py.start()
+    print(
+        """
+    ------------------
+   | Userbot Started! POWERED BY @SANKIXWORLD |
+    ------------------
+"""
+    )
+    await idle()
+    await arq.close()
+
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
